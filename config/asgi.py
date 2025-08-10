3ab1e4c5a020dd1b49b87bd5d56d2e4c8b0879ac
Hatef Barin <amir.barin76@gmail.com>
2025-08-11 03:23:30 +0430

    chore: update ASGI application for WebSocket support

--- config/asgi.py
+++ config/asgi.py
@@ -1,16 +1,19 @@
-"""
-ASGI config for config project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
-"""
-
 import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
 
-application = get_asgi_application()
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+django_app = get_asgi_application()
+
+# add chat.routing.websocket_urlpatterns in Phase 3
+application = ProtocolTypeRouter({
+    "http": django_app,
+    "websocket": AuthMiddlewareStack(
+        URLRouter([])  # placeholder until chat app is added
+    ),
+})
