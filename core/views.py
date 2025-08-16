f3e2c434fb5030477da63065dc0d2f23db525cf9
Hatef Barin <amir.barin76@gmail.com>
2025-08-17 00:06:06 +0430

    chore(core): add ProfileViewSet and UserMeViewSet for user profile and user data management

--- core/views.py
+++ core/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .models import User, Profile
+from .serializers import ProfileSerializer, UserMeSerializer
+
+
+
+class ProfileViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing user profiles.
+    """
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """
+        Optionally restricts the returned profiles to the user’s profile,
+        by filtering against the authenticated user.
+        """
+        return self.queryset.filter(user=self.request.user)
+
+
+
+class UserMeViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for the /me endpoint, allowing users to view and update their information.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserMeSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        """
+        This will ensure we always access the authenticated user.
+        """
+        return self.request.user
+
+    def perform_update(self, serializer):
+        """
+        Handle the saving of the user and profile when updating.
+        """
+        user = self.get_object()
+        serializer.save(user=user)
+        super().perform_update(serializer)
