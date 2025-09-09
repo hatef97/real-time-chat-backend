456d9078ee454fe48643826eb5f9301178f22bf3
Hatef Barin <amir.barin76@gmail.com>
2025-09-09 17:26:26 +0430

    test(models): align Message test with chat_room/sender/content fields

---
+++ tests/test_models.py
@@ -0,0 +1,18 @@
+import pytest
+
+from chat.models import ChatRoom, Message
+from core.models import User
+
+
+
+@pytest.mark.django_db
+def test_make_message():
+    u = User.objects.create_user(username="bob")
+    room = ChatRoom.objects.create(name="lobby")
+
+    msg = Message.objects.create(chat_room=room, sender=u, content="hi")
+
+    assert msg.chat_room_id == room.id
+    assert msg.sender_id == u.id
+    assert msg.content == "hi"  
+    
