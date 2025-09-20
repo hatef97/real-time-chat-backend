7d8263bc941bdc4d66ea31262ba4567c8306ce7c
Hatef Barin <amir.barin76@gmail.com>
2025-09-20 16:05:39 +0430

    fix(static): restore Django styles by using staticfiles finders

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from djoser.views import UserViewSet
 
@@ -18,3 +20,6 @@ urlpatterns = [
     # chat app
     path('api/chat/', include(("chat.urls", "chat"), namespace="chat")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
