f1f5c8f3302487a5cafe492558b18a919fd05cfc
Hatef Barin <amir.barin76@gmail.com>
2025-09-21 13:14:22 +0430

    feat(core): add signals to auto-create Profile and update last_seen via Presence

--- core/signals.py
+++ core/signals.py
@@ -1,5 +1,7 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.utils import timezone
+from django.conf import settings
 
 from .models import Profile, User
 
@@ -16,3 +18,11 @@ def create_user_profile(sender, instance, created, **kwargs):
 def save_user_profile(sender, instance, **kwargs):
     if hasattr(instance, "profile"):
         instance.profile.save()
+
+
+
+from chat.models import Presence
+@receiver(post_save, sender=Presence)
+def touch_user_last_seen(sender, instance, **kwargs):
+    User.objects.filter(id=instance.user_id).update(last_login=timezone.now())
+    
